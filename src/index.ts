eaf3f10f9f397bdfad6085b501f4ad62f28d9d1f
Cameron Little <cameron@camlittle.com>
2023-11-29 17:51:29 -0700

    Fix platform setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { API } from "homebridge";
 import { MideaPlatform } from "./MideaPlatform";
+import { PLATFORM_NAME } from "./settings";
 
 export = (api: API) => {
-  api.registerPlatform("homebridge-midea-air", "midea-air", MideaPlatform);
+  api.registerPlatform(PLATFORM_NAME, MideaPlatform);
 };
